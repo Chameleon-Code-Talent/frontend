8e5641a5e7ba20c307927bc22e4bced441aa8065
Matheus Lima <teulima20@hotmail.com>
2024-01-17 20:48:39 -0300

    feat: created aboutMe component

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -2,7 +2,14 @@ import styles from "./aboutMe.module.css"
 
 const AboutMe = () => {
     return (
-        <h1 className={styles.sobre}>Sobre mim</h1>
+        <div className={styles.container}>
+            <div>
+                <h2 className={styles.titulo}>Sobre mim</h2>
+            </div>
+            <div>
+                <p className={styles.texto}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+            </div>
+        </div>
     )
 }
 
