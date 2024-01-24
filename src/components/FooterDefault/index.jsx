d8bb2e81bce28075645846b8bdf76cd967001289
Matheus Lima <teulima20@hotmail.com>
2024-01-24 18:47:06 -0300

    feat: created footer default in login page

---
+++ src/components/FooterDefault/index.jsx
@@ -0,0 +1,21 @@
+import styles from "./footerDefault.module.css"
+
+import logoCamaleao from "../../assets/logoCamaleao.svg"
+
+const FooterDefault = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.containerCCT}>
+                <img src={logoCamaleao} alt="Icone" />
+                <h4>© <strong style={{ color: "#00DF5E" }}>Chameleon Code Talent</strong> 2024 - Todos os direitos reservados</h4>
+            </div>
+
+            <div className={styles.containerTermos}>
+                <p>Política de Privacidade</p>
+                <p>Termos de uso</p>
+            </div>
+        </section>
+    )
+}
+
+export default FooterDefault
--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -1,17 +1,22 @@
 import CardBenefits from "../../components/CardBenefits";
+import FooterDefault from "../../components/FooterDefault";
 import FormLogin from "../../components/FormLogin";
 import styles from "./singIn.module.css";
 
 const SingIn = () => {
     return (
-        <section>
-            <div className={styles.containerLogin}>
-                <FormLogin/>
-            </div>
-            <div className={styles.containerVantagens}>
-                <CardBenefits />
-            </div>
-        </section>
+        <div>
+            <section>
+                <div className={styles.containerLogin}>
+                    <FormLogin/>
+                </div>
+                <div className={styles.containerVantagens}>
+                    <CardBenefits />
+                </div>
+            </section>
+
+            <FooterDefault />
+        </div>
     )
 }
 
