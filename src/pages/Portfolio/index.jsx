62904d6c7cce68b721e1287a567f7495293b5210
othonss <othon.santos.silva@gmail.com>
2024-01-30 20:40:41 -0300

    style: Adjusted element spacing and included the main tag of the my portfolio page

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -6,17 +6,20 @@ import MyProjects from "../../components/MyProjects";
 import MyServices from "../../components/MyServices";
 import MySkills from "../../components/MySkills";
 import PersonInfo from "../../components/PersonInfo";
+import styles from "./portfolio.module.css";
 
 const Portfolio = () => {
   return (
     <>
       <HeaderPortfolio />
-      <MyPresentation />
-      <AboutMe />
-      <PersonInfo />
-      <MyProjects />
-      <MyServices />
-      <MySkills />
+      <main className={styles.container}>
+        <MyPresentation />
+        <AboutMe />
+        <PersonInfo />
+        <MyProjects />
+        <MyServices />
+        <MySkills />
+      </main>
       <Footer />
     </>
   );
