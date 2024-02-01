4e8c8e5cfa0d239aee2c66a246ffc5a60b68cffe
othonss <othon.santos.silva@gmail.com>
2024-02-01 17:14:37 -0300

    style: adjusted the spacing of personal information elements

--- src/components/PersonInfo/index.jsx
+++ src/components/PersonInfo/index.jsx
@@ -7,43 +7,43 @@ import phoneIcon from "../../assets/phone.svg";
 const PersonInfo = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.containerIconNome}>
+      <div className={styles.containerIcon}>
         <div className={styles.emoji}>
           <img src={smileIcon} alt="Icone para meu nome" />
         </div>
-        <div>
-          <p className={styles.titulo}>Meu nome</p>
-          <p className={styles.texto}>Luri Silva</p>
+        <div className={styles.texts}>
+          <span className={styles.titulo}>Meu nome</span>
+          <span className={styles.texto}>Luri Silva</span>
         </div>
       </div>
 
-      <div className={styles.containerIconEmail}>
+      <div className={styles.containerIcon}>
         <div className={styles.emoji}>
           <img src={mailIcon} alt="Icone para o e-mail" />
         </div>
-        <div>
-          <p className={styles.titulo}>E-mail</p>
-          <p className={styles.texto}>luricold99@gmail.com</p>
+        <div className={styles.texts}>
+          <span className={styles.titulo}>E-mail</span>
+          <span className={styles.texto}>luricold99@gmail.com</span>
         </div>
       </div>
 
-      <div className={styles.containerIconGithub}>
+      <div className={styles.containerIcon}>
         <div className={styles.emoji}>
           <img src={vectorIcon} alt="Icone para o github" />
         </div>
-        <div>
-          <p className={styles.titulo}>Github</p>
-          <p className={styles.texto}>@luricode</p>
+        <div className={styles.texts}>
+          <span className={styles.titulo}>Github</span>
+          <span className={styles.texto}>@luricode</span>
         </div>
       </div>
 
-      <div className={styles.containerIconPhone}>
+      <div className={styles.containerIcon}>
         <div className={styles.emoji}>
           <img src={phoneIcon} alt="Icone para o telefone" />
         </div>
-        <div>
-          <p className={styles.titulo}>Telefone</p>
-          <p className={styles.texto}>(14) 99999-9999</p>
+        <div className={styles.texts}>
+          <span className={styles.titulo}>Telefone</span>
+          <span className={styles.texto}>(14) 99999-9999</span>
         </div>
       </div>
     </div>
